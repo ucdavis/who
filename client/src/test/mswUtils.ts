fcbdf1fc3ef5524cda1d322063a2c26c73c24171
Jason Sylvestre <jsylvestre@ucdavis.edu>
2026-08-19 10:06:01 -0700

    rabbit fixes. and others

--- client/src/test/mswUtils.ts
+++ client/src/test/mswUtils.ts
@@ -3,22 +3,20 @@ import { setupServer } from 'msw/node';
 import { afterAll, afterEach, beforeAll } from 'vitest';
 
 // Create a shared MSW server instance that can be used across all tests
-export const testServer = setupServer(
+export const server = setupServer(
   http.get('/api/app-info', () => HttpResponse.json({ provider: 'Rosetta' }))
 );
 
 // Global setup for MSW server
 // This will be automatically called when imported in test files
 beforeAll(() => {
-  testServer.listen({ onUnhandledRequest: 'error' });
+  server.listen({ onUnhandledRequest: 'error' });
 });
 
 afterEach(() => {
-  testServer.resetHandlers();
+  server.resetHandlers();
 });
 
 afterAll(() => {
-  testServer.close();
+  server.close();
 });
-
-export { testServer as server };
